6aa67e49fec24b4d81d06159ae07ead4b4b8488a
TebionDL <purreren@gmail.com>
2025-01-23 22:44:37 +0100

    Added token authorization

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify, render_template, send_from_directory
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
 import os
 import torch
 from database import init_db, insert_question, get_questions
-
+from dotenv import load_dotenv
+load_dotenv()
 
 try:
     # Load model
@@ -28,8 +30,19 @@ except Exception as e:
 # Start flask app
 app = Flask(__name__)
 
+try:
+    # Obviously in production, this key must be secret, but for this example it will be public
+    app.config["JWT_SECRET_KEY"] = os.getenv("MY_SECRET_KEY")
+    jwt = JWTManager(app)
+    
+except KeyError:
+    raise RuntimeError("JWT_SECRET_KEY environment variable not set")
+
 # Initialize the database
-init_db()
+try:
+    init_db()
+except Exception as e:
+    raise RuntimeError(f"Error initializing the database: {e}")
 
 '''
 Request Type: POST
@@ -37,6 +50,7 @@ Input Data: {"prompt": "prompt for the generative model"}
 Output Data: {"generated_text": "answer of the model"}
 '''
 @app.route("/generate", methods=["POST"])
+@jwt_required()
 def generate_text():
     try:
         # Get data from the request
@@ -65,6 +79,7 @@ Request Type: GET
 Output Data: {"historic": [["prompt", "answer of the model"], ["prompt2", "second answer of the model"], ...]}
 '''
 @app.route("/historic", methods=["GET"])
+@jwt_required()
 def get_historic():
     try:
         # Get data from the database
@@ -82,6 +97,7 @@ Input Data: {"*": "Any existant parameter for an specific model configuration"}
 Output Data: {"A good message if the configuration was applied"}
 '''
 @app.route("/change_config", methods=["POST"])
+@jwt_required()
 def change_config():
     global pipe, model, tokenizer
     try:
@@ -107,6 +123,15 @@ def change_config():
     
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+
+@app.route('/login', methods=['GET'])
+def login():
+    
+    #For simplicity, this method will not check any credentials, just give the token
+    
+    access_token = create_access_token(identity="admin")
+    return jsonify(access_token=access_token)
     
 @app.route('/docs')
 def swagger_ui():
